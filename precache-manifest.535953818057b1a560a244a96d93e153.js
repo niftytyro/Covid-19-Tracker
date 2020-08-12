0da8ced5aa753f85bd0471b78495f22e31bdb24d
udasitharani <udasitharani@users.noreply.github.com>
2020-08-12 19:36:06 +0000

    deploy: 01c4c80b6a9878bd71170c408eaf5619724681c4

---
+++ precache-manifest.535953818057b1a560a244a96d93e153.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbcce3191e7c783663fba851a9b18bc0",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "779359478927b99c58a5",
+    "url": "/Covid-19-Tracker/static/css/main.f9168f34.chunk.css"
+  },
+  {
+    "revision": "d8a1fb2d63b4414bfb55",
+    "url": "/Covid-19-Tracker/static/js/2.e93ca93f.chunk.js"
+  },
+  {
+    "revision": "5d0309012a0361d740c526e70b42129d",
+    "url": "/Covid-19-Tracker/static/js/2.e93ca93f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "779359478927b99c58a5",
+    "url": "/Covid-19-Tracker/static/js/main.7019fff5.chunk.js"
+  },
+  {
+    "revision": "4b644fa9ca0287604fad",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.b2c8fe1b.js"
+  }
+]);
